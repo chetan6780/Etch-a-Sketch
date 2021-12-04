10ed890fcc1ef94ca32bb85e1383ff9ba146f9ca
chetan6780 <crp20802@gmail.com>
2021-12-04 12:44:14 +0530

    continue...

--- js/script.js
+++ js/script.js
@@ -1,17 +1,48 @@
 const grid = document.getElementById("grid");
+const gridRange = document.getElementById("range-input");
+const span = document.getElementsByClassName("span");
 
 makeRows = (rows, cols) => {
     grid.style.setProperty('--grid-rows', rows);
     grid.style.setProperty('--grid-cols', cols);
     for (c = 0; c < (rows * cols); c++) {
         let box = document.createElement("div");
-        box.innerHTML = (" ");
-        grid.appendChild(box).className = "grid-item";
+        box.className = "grid-item";
+        grid.appendChild(box);
     };
 };
 
+colorChange = (e) => {
+    console.log(e.target.style);
+}
+clearGrid = () => {
+    grid.innerHTML = ''
+}
+
+gridRange.onmousemove = () => {
+    let n = gridRange.value;
+    clearGrid();
+    span[0].innerHTML = n;
+    span[1].innerHTML = n;
+    makeRows(n, n);
+};
+
+gridItem.onmouseover = () => {
+    console.log(gridItem.style);
+}
+
+
+window.onload = () => {
+    // const n = prompt("Enter number between 1 and 100");
+    console.log(gridRange);
+    makeRows(16, 16);
+    let gridItem = document.getElementsByClassName("grid-item")
+    console.log(gridItem);
+
+}
 // makeRows(16, 16);
-makeRows(32, 32);
+// makeRows(32, 32);
+// makeRows(n, n);
 // makeRows(64, 64);
 // makeRows(256, 256);
 // makeRows(10, 10);
